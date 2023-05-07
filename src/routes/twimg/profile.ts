c5c96bca5bdfa7fdbb9c03ff1d37a8ca785bf2e6
madeline-xoxo <needhelpwithrift@gmail.com>
2023-05-07 00:07:41 +0000

    fix `undefined` error

--- src/routes/twimg/profile.ts
+++ src/routes/twimg/profile.ts
@@ -7,14 +7,14 @@ const router = express.Router();
 router.get("/profile_images/*", (req, res) => {
 	const [image] = req.url.split("/").slice(-1);
 	return res.sendFile(`${rootPath}/img/${image}`, (e) => {
-		error(e);
+		error(e || "An unknown error relating to Twimg stub occurred");
 	});
 });
 
 router.get("/images/*", (req, res) => {
 	const [image] = req.url.split("/").slice(-1);
 	return res.sendFile(`${rootPath}/img/${image}`, (e) => {
-		error(e);
+		error(e || "An unknown error relating to Twimg stub occurred");
 	});
 });
 
