f9eec136c41aaf3c1fa96b04c8914851c10ab2a1
madeline-xoxo <needhelpwithrift@gmail.com>
2023-05-07 01:14:14 +0000

    fixed a bug which froze the frontend upon unsuccessful gql

--- src/routes/gql/graphQlHandler.ts
+++ src/routes/gql/graphQlHandler.ts
@@ -32,6 +32,11 @@ router.use("*", async (req, res) => {
 		}
 	}
 	warn(`GraphQL query sent to ${query} failed!`);
+	return res
+		.status(200)
+		.send({
+			msg: "This GraphQL endpoint has not yet been implemented. Pray to God that this stub is enough, and submit an issue on GitHub.",
+		});
 });
 
 export default router;
